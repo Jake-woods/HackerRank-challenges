9b9809af2d3b625e8be47ee328dc81c5159a941e
Jake-woods <jake.13.5.2016@gmail.com>
2019-07-02 10:10:13 +0100

    Day 5 - Loops

--- Day 5/challenge.js
+++ Day 5/challenge.js
@@ -26,4 +26,8 @@ function readLine() {
 
 function main() {
    const n = parseInt(readLine(), 10);
+
+   for (let i = 1; i <= 10; i++) {
+      console.log(n + " x " + i + " = " + (n * i));
+   }
 }
